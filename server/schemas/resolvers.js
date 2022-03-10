f10c3a8276ceddd0f6bf8dc0a4215a3c2c70943b
sboucher <sboucher2011@gmail.com>
2022-03-10 16:02:13 -0500

    updated sever folder for graphQL

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,9 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { model } = require('mongoose');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
-
 const resolvers = {
-
-};
-
-module.exports = resolvers;
+    Query: {
+      helloWorld: () => {
+        return 'Hello world!';
+      }
+    }
+  };
+  
+  module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,12 @@
-const {gql} = require('apollo-server-express');
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
 
+// create our typeDefs
 const typeDefs = gql`
-
+  type Query {
+    helloWorld: String
+  }
 `;
 
+// export the typeDefs
 module.exports = typeDefs;
